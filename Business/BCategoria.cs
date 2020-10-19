198080690a4b71cb67df61cc91f10574505dbca1
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-10-19 18:51:41 -0500

    Agregar archivos de proyecto.

---
+++ Business/BCategoria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+
+namespace Business
+{
+    public class BCategoria
+    {
+        private DCategoria dCategoria = null;
+        public List<Categoria> GetCategoriasTop5(string NombreCategoria)
+        {
+            
+            List<Categoria> categorias = null;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                categorias = dCategoria.GetCategorias(NombreCategoria).Take(5).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return categorias;
+        }
+        public List<Categoria> GetCategoriasconSlash()
+        {
+
+            List<Categoria> categorias = null;
+
+            try
+            {
+                dCategoria = new DCategoria();
+                categorias = dCategoria.GetCategorias("/");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return categorias;
+        }        
+    }
+}
---
+++ Data/DCategoria.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace Data
+{
+   public class DCategoria
+    {
+
+     public   List<Categoria> GetCategorias(string nombreCategoria)
+        {
+            //Declarar
+            List<Categoria> categorias = null;
+            string commandText = string.Empty;
+            SqlParameter[] parameters = null;
+
+            try
+            {
+                //Instanciar
+                commandText = "USP_ListarCategorias";
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@NombreCategoria", System.Data.SqlDbType.VarChar);
+                parameters[0].Value = nombreCategoria;
+                categorias = new List<Categoria>();
+
+                //Porque se utiliza automaticamente se borra de la memoria.
+                using (SqlDataReader reader = 
+                    SqlHelper.ExecuteReader(SqlHelper.Connection, commandText, 
+                    System.Data.CommandType.StoredProcedure, parameters))
+                    //SQL Helper te asegura que se van a cerrar siempre las conexiones
+                {
+                    while (reader.Read())
+                    {
+                        categorias.Add(new Categoria
+                        {
+                            IdCategoria = reader["idcategoria"] != null ? Convert.ToInt32(reader["idcategoria"]) : 0,
+                            NombreCategoria = reader["nombrecategoria"] != null ? Convert.ToString(reader["nombrecategoria"]) : "",
+                            Descripcion = reader["descripcion"] != null ? Convert.ToString(reader["descripcion"]) : "",
+                        });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return categorias;
+        }
+    }
+}
